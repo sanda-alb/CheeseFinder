130c75ad787c4248d9962e7b8b1f643be4b3cdbd
Sanda Albrecht <sanda_albrecht@Deniss-MacBook-Pro.local>
2021-10-11 18:14:45 +0300

    Create an Observable

--- app/src/main/java/com/raywenderlich/android/cheesefinder/CheeseActivity.kt
+++ app/src/main/java/com/raywenderlich/android/cheesefinder/CheeseActivity.kt
@@ -30,6 +30,37 @@
 
 package com.raywenderlich.android.cheesefinder
 
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.activity_cheeses.*
+
 class CheeseActivity : BaseSearchActivity() {
+
+    private fun createButtonClickObservable(): Observable<String> {
+
+        return Observable.create { emitter ->
+
+            searchButton.setOnClickListener {
+
+                emitter.onNext(queryEditText.text.toString())
+            }
+
+            emitter.setCancellable {
+
+                searchButton.setOnClickListener(null)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val searchTextObservable = createButtonClickObservable()
+
+        searchTextObservable
+            .subscribe { query ->
+
+                cheeseSearchEngine.search(query)?.let { showResult(it) }
+            }
+    }
 
 }
--- app/src/main/java/com/raywenderlich/android/cheesefinder/database/CheeseDatabase.kt
+++ app/src/main/java/com/raywenderlich/android/cheesefinder/database/CheeseDatabase.kt
@@ -37,26 +37,29 @@ import android.content.Context
 @Database(entities = [(Cheese::class)], version = 1)
 abstract class CheeseDatabase : RoomDatabase() {
 
-  abstract fun cheeseDao(): CheeseDao
+    abstract fun cheeseDao(): CheeseDao
 
-  companion object {
+    companion object {
 
-    private var INSTANCE: CheeseDatabase? = null
+        private var INSTANCE: CheeseDatabase? = null
 
-    fun getInstance(context: Context): CheeseDatabase {
-      if (INSTANCE == null) {
-        synchronized(this) {
-          INSTANCE = Room.databaseBuilder(context,
-              CheeseDatabase::class.java, "cheese.db")
-              .build()
+        fun getInstance(context: Context): CheeseDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(
+                        context,
+                        CheeseDatabase::class.java, "cheese.db"
+                    )
+                        .allowMainThreadQueries()
+                        .build()
+                }
+            }
+            return INSTANCE as CheeseDatabase
         }
-      }
-      return INSTANCE as CheeseDatabase
-    }
 
-    fun destroyInstance() {
-      INSTANCE = null
-    }
+        fun destroyInstance() {
+            INSTANCE = null
+        }
 
-  }
+    }
 }
